59394f72fc585e1412a8817d35a1439092e341c8
Badar Hossain <badar12041@gmail.com>
2024-02-04 18:30:39 +0600

    Function practice task

---
+++ practice_function.js
@@ -0,0 +1,28 @@
+// Task-1
+
+function addNumbers(a,b,c,d){
+    return a +b+c+d;
+
+}
+
+// console.log(addNumbers(1,2,3,4));
+
+
+// Task-4
+
+function binaryString(arr){
+    let count = 0;
+    for(const item of arr){
+        if(item == 0){
+            count++;
+        }
+        else{
+            continue;
+        }
+    }
+    return count;
+}
+
+const arr = '10010010100';
+// console.log(binaryString(arr));
+
